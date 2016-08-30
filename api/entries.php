4939189c2d2b0d740933647682383fefff3a78f1
Stu Perry <stewart.c.perry@gmail.com>
2016-08-30 07:41:47 +0100

    change date arithmetic to use DateTime and DateInterval to avoid DST issues; fix some problems with slot filling algo; implement cutoff time for tokens

--- api/entries.php
+++ api/entries.php
@@ -4,7 +4,7 @@ include "utils.inc";
 
 # Return booking entries in JSON format
 
-if ($_SERVER['REQUEST_METHOD'] != 'GET') 
+if ($_SERVER['REQUEST_METHOD'] != 'GET' && $_SERVER['REQUEST_METHOD'] != 'OPTIONS') 
 {
     return_error(405, "method not allowed");
 }
@@ -29,31 +29,55 @@ include "../functions.inc";
 include "../$dbsys.inc";
 include "../mrbs_auth.inc";
 
-$SECONDS_PER_DAY = 24 * 60 * 60;
+$timezone = new DateTimeZone('Europe/London');
 
-function truncate_to_midnight($ts)
+function to_midnight($adate)
 {
-    global $SECONDS_PER_DAY;
-    return $SECONDS_PER_DAY * intval($ts / $SECONDS_PER_DAY);
+    $dt = clone $adate;
+    $dt->setTime(0, 0, 0);
+    return $dt;
 }
 
-$now_timestamp = time();
-$today_timestamp = truncate_to_midnight($now_timestamp);
+function dt_from_iso_str($datestr)
+{
+    global $timezone;
+    $dt = DateTime::createFromFormat("Y-m-d", $datestr, $timezone);
+    $dt->setTime(0, 0, 0);
+    return $dt;
+}
+
+function to_minutes($date)
+{
+    $hours = intval($date->format("H"));
+    $mins  = intval($date->format("i"));
+    return 60 * $hours + $mins;
+}
+
+function adjust_dt($dt, $days)
+{
+    $interval = new DateInterval("P" . $days . "D");
+    $dt->add($interval);
+}
+
+$now_dt = new DateTime(null, $timezone);
+$today_dt = to_midnight($now_dt);
 $area = get_default_area();
-$timezone = new DateTimeZone('Europe/London');
+$cutoff_dt = clone $now_dt;
+adjust_dt($cutoff_dt, $advance_limit);
 
 if (isset($start_date))
 {
-    $start_timestamp = truncate_to_midnight(DateTime::createFromFormat("Y-m-d", $start_date, $timezone)->getTimestamp());    
+    $start_dt = dt_from_iso_str($start_date);    
 } else {
-    $start_timestamp = $today_timestamp;
+    $start_dt = $today_dt;
 }
 
 if (isset($end_date))
 {
-    $end_timestamp = truncate_to_midnight(DateTime::createFromFormat("Y-m-d", $end_date, $timezone)->getTimestamp());
+    $end_dt = dt_from_iso_str($end_date);
 } else {
-    $end_timestamp = $start_timestamp + ($advance_limit + 1) * $SECONDS_PER_DAY;
+    $end_dt = clone $start_dt;    
+    adjust_dt($end_dt, $advance_limit+1);
 }
 
 
@@ -64,7 +88,8 @@ $sql = "SELECT $tbl_room.id, start_time, end_time, name, $tbl_entry.id, type,
    FROM $tbl_entry, $tbl_room
    WHERE $tbl_entry.room_id = $tbl_room.id
    AND area_id = $area
-   AND start_time <= $end_timestamp AND start_time > $start_timestamp
+   AND start_time <= " . $end_dt->getTimeStamp() . "
+   AND start_time > "  . $start_dt->getTimeStamp() . "
    ORDER BY start_time";
 
 $res = sql_query($sql);
@@ -78,14 +103,17 @@ for ($i = 0; ($row = sql_row($res, $i)); $i++)
 {
     $room_id    = $row[0];
     $start      = $row[1];
-    $start_date = date("Y-m-d", $start);
-    $start_time = date("H:i",   $start);
+    $start_dt   = new DateTime(null, $timezone);
+    $start_dt->setTimestamp($start);
+    $start_date = $start_dt->format("Y-m-d");
+    $start_time = $start_dt->format("H:i");
+    $start_mins = to_minutes($start_dt);
     $duration   = $row[2] - $start;
     $date_list  = &retrieve_or_create($result, $start_date);
     $room_slots = &retrieve_or_create($date_list, $room_id);
 
     $slot = array("start_time" => $start_time, 
-                  "start_mins" => ($start % $SECONDS_PER_DAY) / 60,
+                  "start_mins" => $start_mins,
                   "duration_mins" => $duration / 60, 
                   "name" => $row[3],
                   "id" => intval($row[4]),
@@ -98,7 +126,7 @@ for ($i = 0; ($row = sql_row($res, $i)); $i++)
 
 function add_free_slots($date, &$day_bookings, $room)
 {
-    global $morningstarts, $morningstarts_minutes, $eveningends, $eveningends_minutes, $resolution, $stagger_set, $now_timestamp;
+    global $morningstarts, $morningstarts_minutes, $eveningends, $eveningends_minutes, $resolution, $stagger_set, $now_dt, $cutoff_dt;
     $resolution_mins  = $resolution / 60;
     $room_offset_mins = ($room-1) * $resolution_mins;
     $start_mins       = $morningstarts * 60 + $morningstarts_minutes + $room_offset_mins; 
@@ -109,51 +137,64 @@ function add_free_slots($date, &$day_bookings, $room)
     reset($room_slots);
     $nbookings  = count($room_slots);
     $idx = 0;
+    $start = $start_mins; 
+    $start_dt = clone $date;
+    $start_date = $start_dt->format("Y-m-d");
 
-    for ($start = $start_mins; $start < $end_mins; $start += $default_duration) 
+    while ($start < $end_mins) 
     {
-        $start_time = sprintf("%02d:%02d", $start/60, $start%60);
+        $start_dt->setTime($start/60, $start%60, 0);
+        $start_time = $start_dt->format("H:i");
         if (isset($room_slots[$start_time]))
         {
             $last_slot = $room_slots[$start_time];
+            $start += $last_slot["duration_mins"];
             continue;
         }
-        if (isset($last_slot) && $start < ($last_slot["start_mins"] + $last_slot["duration_mins"]))
-            continue;
         $next_start = $end_mins;
         while ($idx < $nbookings)
         {
             $next_slot = current($room_slots);
-            next($room_slots);
-            ++$idx;
             if ($next_slot["start_mins"] > $start)
             {
                 $next_start = $next_slot["start_mins"];
                 break;
             }
+            next($room_slots);
+            ++$idx;
         }
-        $duration = min($default_duration, $next_start - $start);
+        $duration_mins = $next_start - $start;
+        if ($duration_mins > $default_duration) 
+        {
+            # try to align with the normal slots for this court
+            $remainder = ($start - $start_mins) % $default_duration;
+            $duration_mins = $default_duration - $remainder;
+        }
 
         $gap = array("start_mins" => $start,
-                     "duration_mins" => $duration,
+                     "duration_mins" => $duration_mins,
                      "start_time" => $start_time);
-        if ($start > $now_timestamp)
-            $gap["token"] = createToken(sprintf("%sT%s", $date, $start_time), $room);
+        if ($start_dt > $now_dt  && $start_dt < $cutoff_dt)
+            $gap["token"] = createToken(sprintf("%sT%s", $start_date, $start_time), $room);
         $room_slots[$start_time] = $gap;
+        $start += $duration_mins;
     }
 }
         
 
 if (isset($with_tokens))
 {
-    for ($date_ts = $start_timestamp; $date_ts < $end_timestamp; $date_ts += $SECONDS_PER_DAY)
+    $iter_dt = clone $start_dt;
+    $one_day = new DateInterval("P1D");
+    while ($iter_dt < $end_dt)
     {
-        $date = date("Y-m-d", $date_ts);
+        $date = $iter_dt->format("Y-m-d");
         $date_list  = &retrieve_or_create($result, $date);
         for ($room = 1; $room <= 3; ++$room) 
         {
-            add_free_slots($start_date, $date_list, $room); 
+            add_free_slots($iter_dt, $date_list, $room); 
         }
+        $iter_dt->add($one_day);
     }
 }
 
