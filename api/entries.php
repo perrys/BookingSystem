0ff9cbe9a3ff655659d7e1e8c49fb5e6b24c2eb1
Stu Perry <stewart.c.perry@gmail.com>
2016-11-30 09:01:42 +0000

    refactor to move logic related to GET (list) request inside a function; move util functions to utils.inc

--- api/entries.php
+++ api/entries.php
@@ -29,100 +29,82 @@ include "../functions.inc";
 include "../$dbsys.inc";
 include "../mrbs_auth.inc";
 
+# some useful dates:
+
 $timezone = new DateTimeZone('Europe/London');
-
-function to_midnight($adate)
-{
-    $dt = clone $adate;
-    $dt->setTime(0, 0, 0);
-    return $dt;
-}
-
-function dt_from_iso_str($datestr)
-{
-    global $timezone;
-    $dt = DateTime::createFromFormat("Y-m-d", $datestr, $timezone);
-    $dt->setTime(0, 0, 0);
-    return $dt;
-}
-
-function to_minutes($date)
-{
-    $hours = intval($date->format("H"));
-    $mins  = intval($date->format("i"));
-    return 60 * $hours + $mins;
-}
-
-function adjust_dt($dt, $days)
-{
-    $interval = new DateInterval("P" . $days . "D");
-    $dt->add($interval);
-}
-
 $now_dt = new DateTime(null, $timezone);
-$today_dt = to_midnight($now_dt);
-$area = get_default_area();
+
 $cutoff_dt = clone $now_dt;
 adjust_dt($cutoff_dt, $advance_limit);
 
+$today_dt =  clone $now_dt;
+$today_dt->setTime(0, 0, 0); # midnight
+
 if (isset($start_date))
 {
-    $start_dt = dt_from_iso_str($start_date);    
+    $start_dt = dt_from_iso_str($start_date, $timezone);    
 } else {
     $start_dt = $today_dt;
 }
 
 if (isset($end_date))
 {
-    $end_dt = dt_from_iso_str($end_date);
+    $end_dt = dt_from_iso_str($end_date, $timezone);
 } else {
     $end_dt = clone $start_dt;    
     adjust_dt($end_dt, $advance_limit+1);
 }
+    
 
-
-$result = array();
-
-$sql = "SELECT $tbl_room.id, start_time, end_time, name, $tbl_entry.id, type,
-        $tbl_entry.description, $tbl_entry.create_by, $tbl_entry.timestamp
-   FROM $tbl_entry, $tbl_room
-   WHERE $tbl_entry.room_id = $tbl_room.id
-   AND area_id = $area
-   AND start_time <= " . $end_dt->getTimeStamp() . "
-   AND start_time > "  . $start_dt->getTimeStamp() . "
-   ORDER BY start_time";
-
-$res = sql_query($sql);
-if (! $res) 
+function &get_entries($start_date, $end_date)
 {
-    trigger_error(sql_error());
-    return_error(500, "internal server error");
-}
+    global $timezone, $tbl_entry, $tbl_room, $start_dt, $end_dt;
+    $area = get_default_area();
 
-for ($i = 0; ($row = sql_row($res, $i)); $i++) 
-{
-    $room_id    = $row[0];
-    $start      = $row[1];
-    $slot_dt    = new DateTime(null, $timezone);
-    $slot_dt->setTimestamp($start);
-    $start_date = $slot_dt->format("Y-m-d");
-    $start_time = $slot_dt->format("H:i");
-    $start_mins = to_minutes($slot_dt);
-    $duration   = $row[2] - $start;
-    $date_list  = &retrieve_or_create($result, $start_date);
-    $room_slots = &retrieve_or_create($date_list, $room_id);
-
-    $slot = array("start_time" => $start_time, 
-                  "start_mins" => $start_mins,
-                  "duration_mins" => $duration / 60, 
-                  "name" => $row[3],
-                  "id" => intval($row[4]),
-                  "type" => $row[5],
-                  "description" => $row[6],
-                  "created_by" => $row[7],
-                  "timestamp" => $row[8]);
-
-    $room_slots[$start_time] = $slot;
+    $result = array();
+    
+    $sql = "SELECT $tbl_room.id, start_time, end_time, name, $tbl_entry.id, type,
+            $tbl_entry.description, $tbl_entry.create_by, $tbl_entry.timestamp
+       FROM $tbl_entry, $tbl_room
+       WHERE $tbl_entry.room_id = $tbl_room.id
+       AND area_id = $area
+       AND start_time <= " . $end_dt->getTimeStamp() . "
+       AND start_time > "  . $start_dt->getTimeStamp() . "
+       ORDER BY start_time";
+    
+    $res = sql_query($sql);
+    if (! $res) 
+    {
+        trigger_error(sql_error());
+        return_error(500, "internal server error");
+    }
+    
+    for ($i = 0; ($row = sql_row($res, $i)); $i++) 
+    {
+        $room_id    = $row[0];
+        $start      = $row[1];
+        $slot_dt    = new DateTime(null, $timezone);
+        $slot_dt->setTimestamp($start);
+        $start_date = $slot_dt->format("Y-m-d");
+        $start_time = $slot_dt->format("H:i");
+        $start_mins = to_minutes($slot_dt);
+        $duration   = $row[2] - $start;
+        $date_list  = &retrieve_or_create($result, $start_date);
+        $room_slots = &retrieve_or_create($date_list, $room_id);
+    
+        $slot = array("start_time" => $start_time, 
+                      "start_mins" => $start_mins,
+                      "duration_mins" => $duration / 60, 
+                      "name" => $row[3],
+                      "id" => intval($row[4]),
+                      "type" => $row[5],
+                      "description" => $row[6],
+                      "created_by" => $row[7],
+                      "timestamp" => $row[8]);
+    
+        $room_slots[$start_time] = $slot;
+    }
+    return $result;
 }
 
 function add_free_slots($date, &$day_bookings, $room)
@@ -186,25 +168,29 @@ function add_free_slots($date, &$day_bookings, $room)
         $iter_mins += $duration_mins;
     }
 }
-        
 
-if (isset($with_tokens))
-{
-    $iter_dt = clone $start_dt;
-    $one_day = new DateInterval("P1D");
-    while ($iter_dt < $end_dt)
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{        
+    $result = &get_entries($start_date, $end_date);
+    if (isset($with_tokens))
     {
-        $date = $iter_dt->format("Y-m-d");
-        $date_list  = &retrieve_or_create($result, $date);
-        for ($room = 1; $room <= 3; ++$room) 
-        {
-            add_free_slots($iter_dt, $date_list, $room); 
-        }
-        $iter_dt->add($one_day);
-    }
-}
+        $iter_dt = clone $start_dt;
 
-header("Content-Type: application/json");
-echo json_encode($result);
+        $one_day = new DateInterval("P1D");
+        while ($iter_dt < $end_dt)
+        {
+            $date = $iter_dt->format("Y-m-d");
+            $date_list  = &retrieve_or_create($result, $date);
+            for ($room = 1; $room <= 3; ++$room) 
+            {
+                add_free_slots($iter_dt, $date_list, $room); 
+            }
+            $iter_dt->add($one_day);
+        }
+    }
+    
+    header("Content-Type: application/json");
+    echo json_encode($result);
+}
 
 ?>
